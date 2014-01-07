1890eb2eef406a0981cc81da36325d7cdbd04434
Sotiris Beis <sot.beis@gmail.com>
2014-01-07 13:15:10 +0200

    Find mean value of x-dimension vector from 2d ArrayList function added

--- src/eu/socialsensor/main/Utils.java
+++ src/eu/socialsensor/main/Utils.java
@@ -4,9 +4,52 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
+	
+	public static void main(String args[]) {
+		Utils utils = new Utils();
+		List<Double> list1 = new ArrayList<Double>();
+		list1.add(1.0);
+		list1.add(2.0);
+		list1.add(3.0);
+		list1.add(3.0);
+		List<Double> list2 = new ArrayList<Double>();
+		list2.add(4.0);
+		list2.add(5.0);
+		list2.add(6.0);
+		list2.add(6.0);
+		List<Double> list3 = new ArrayList<Double>();
+		list3.add(7.0);
+		list3.add(8.0);
+		list3.add(9.0);
+		list3.add(9.0);
+		List<List<Double>> data = new ArrayList<List<Double>>();
+		data.add(list1);
+		data.add(list2);
+		data.add(list3);
+		utils.calculateMeanList(data);
+	}
+	
+	/**
+	 * Calculates the mean value of x-dimenstion vector.
+	 * @param data - 2d ArrayList
+	 */
+	public List<Double> calculateMeanList(List<List<Double>> data) {
+		int yDim = data.size();
+		int xDim = data.get(0).size();
+		List<Double> meanData = new ArrayList<Double>();
+		for(int i = 0; i < yDim; i++) {
+			double[] temp = new double[xDim];
+			for(int j = 0; j < xDim; j++) {
+				temp[j] = data.get(i).get(j);
+			}
+			meanData.add(calculateMean(temp));
+		}
+		return meanData;
+	}
 	
 	public double calculateMean(double[] data) {
 		double sum = 0;
