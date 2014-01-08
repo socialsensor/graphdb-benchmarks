8717a29156aabbaa63e6811b27e5a1982d1fb955
Sotiris Beis <sot.beis@gmail.com>
2014-01-08 12:36:12 +0200

    Execution with all possible combinations added

--- src/eu/socialsensor/benchmarks/SingleInsertionBenchmark.java
+++ src/eu/socialsensor/benchmarks/SingleInsertionBenchmark.java
@@ -1,6 +1,7 @@
 package eu.socialsensor.benchmarks;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import eu.socialsensor.singleInsertion.Neo4jSingleInsertion;
@@ -21,59 +22,116 @@ public class SingleInsertionBenchmark {
 	}
 	
 	public void startBenchmark() {
+		System.out.println("###########################################################");
+		System.out.println("############ Starting Single Insertion Benchmark ############");
+		System.out.println("###########################################################");
 		
-		titanSingleInsertionBenchmark();
-//		orientSingleInsertionBenchmark();
-//		neo4jSinglesInsertionBenchmark();
+		List<List<Double>> titanInsertionTimesOfEachScenario = new ArrayList<List<Double>>();
+		List<List<Double>> orientInsertionTimesOfEachScenario = new ArrayList<List<Double>>();
+		List<List<Double>> neo4jInsertionTimesOfEachScenario = new ArrayList<List<Double>>();
+		
+		//Scenario 1
+		System.out.println("##################### Running scenario 1 #####################");
+		titanInsertionTimesOfEachScenario.add(titanSingleInsertionBenchmark());
+		orientInsertionTimesOfEachScenario.add(orientSingleInsertionBenchmark());
+		neo4jInsertionTimesOfEachScenario.add(neo4jSinglesInsertionBenchmark());
+		
+		//Scenario 2
+		System.out.println("##################### Running scenario 2 #####################");
+		titanInsertionTimesOfEachScenario.add(titanSingleInsertionBenchmark());
+		neo4jInsertionTimesOfEachScenario.add(neo4jSinglesInsertionBenchmark());
+		orientInsertionTimesOfEachScenario.add(orientSingleInsertionBenchmark());
+				
+		//Scenario 3
+		System.out.println("##################### Running scenario 3 #####################");
+		orientInsertionTimesOfEachScenario.add(orientSingleInsertionBenchmark());
+		titanInsertionTimesOfEachScenario.add(titanSingleInsertionBenchmark());
+		neo4jInsertionTimesOfEachScenario.add(neo4jSinglesInsertionBenchmark());
+				
+		//Scenario 4
+		System.out.println("##################### Running scenario 4 #####################");
+		orientInsertionTimesOfEachScenario.add(orientSingleInsertionBenchmark());
+		neo4jInsertionTimesOfEachScenario.add(neo4jSinglesInsertionBenchmark());
+		titanInsertionTimesOfEachScenario.add(titanSingleInsertionBenchmark());
+				
+		//Scenario 5
+		System.out.println("##################### Running scenario 5 #####################");
+		neo4jInsertionTimesOfEachScenario.add(neo4jSinglesInsertionBenchmark());
+		titanInsertionTimesOfEachScenario.add(titanSingleInsertionBenchmark());
+		orientInsertionTimesOfEachScenario.add(orientSingleInsertionBenchmark());
+				
+		//Scenario 6
+		System.out.println("##################### Running scenario 6 #####################");
+		neo4jInsertionTimesOfEachScenario.add(neo4jSinglesInsertionBenchmark());
+		orientInsertionTimesOfEachScenario.add(orientSingleInsertionBenchmark());
+		titanInsertionTimesOfEachScenario.add(titanSingleInsertionBenchmark());
+		
+		Utils utils = new Utils();
+		System.out.println("Finding mean values . . . .");
+		List<Double> titanMeanInsertionTimes = utils.calculateMeanList(titanInsertionTimesOfEachScenario);
+		List<Double> orientMeanInsertionTimes = utils.calculateMeanList(orientInsertionTimesOfEachScenario);
+		List<Double> neo4jMeanInsertionTimes = utils.calculateMeanList(neo4jInsertionTimesOfEachScenario);
+		
+		System.out.println("Writing values . . . .");
+		utils.writeTimes(titanMeanInsertionTimes, "data/titanInsertionTimes");
+		utils.writeTimes(orientMeanInsertionTimes, "data/orientInsertionTimes");
+		utils.writeTimes(neo4jMeanInsertionTimes, "data/neo4jInsertionTimes");
+		
+		System.out.println("###########################################################");
+		System.out.println("############ Single Insertion Benchmark Finished ############");
+		System.out.println("######################### RESULTS #########################");
 		
 	}
 	
-	public void titanSingleInsertionBenchmark() {
+	public List<Double> titanSingleInsertionBenchmark() {
 		TitanSingleInsertion titanSingleInsertion = new TitanSingleInsertion();
 		titanSingleInsertion.startup(titanDBDir);
-		List<Long> titanInsertionTimes = titanSingleInsertion.createGraph(datasetDir);
+		List<Double> titanInsertionTimes = titanSingleInsertion.createGraph(datasetDir);
 		titanSingleInsertion.shutdown();
 		try {
-			Thread.sleep(60000);
+			Thread.sleep(6000);
 		} 
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		Utils utils = new Utils();
-		utils.writeTimes(titanInsertionTimes, "data/titanInsertionTimes");
+//		utils.writeTimes(titanInsertionTimes, "data/titanInsertionTimes");
 		utils.deleteRecursively(new File(titanDBDir));
+		return titanInsertionTimes;
 	}
 		
-	public void orientSingleInsertionBenchmark() {
+	public List<Double> orientSingleInsertionBenchmark() {
 		OrientSingleInsertion orientSingleInsertion = new OrientSingleInsertion();
 		orientSingleInsertion.strartup(orientDBDir);
-		List<Long> orientInsertionTimes = orientSingleInsertion.createGraph(datasetDir);
+		List<Double> orientInsertionTimes = orientSingleInsertion.createGraph(datasetDir);
 		orientSingleInsertion.shutdown();
 		try {
-			Thread.sleep(60000);
+			Thread.sleep(6000);
 		} 
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		Utils utils = new Utils();
-		utils.writeTimes(orientInsertionTimes, "data/orientInsertionTimes");
+//		utils.writeTimes(orientInsertionTimes, "data/orientInsertionTimes");
 		utils.deleteRecursively(new File(orientDBDir));
+		return orientInsertionTimes;
 	}
 	
-	public void neo4jSinglesInsertionBenchmark() {
+	public List<Double> neo4jSinglesInsertionBenchmark() {
 		Neo4jSingleInsertion neo4jSingleInsertion = new Neo4jSingleInsertion();
 		neo4jSingleInsertion.startup(neo4jDBDir);
-		List<Long> neo4jInsertionTimes = neo4jSingleInsertion.createGraph(datasetDir);
+		List<Double> neo4jInsertionTimes = neo4jSingleInsertion.createGraph(datasetDir);
 		neo4jSingleInsertion.shutdown();
 		try {
-			Thread.sleep(60000);
+			Thread.sleep(6000);
 		} 
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		Utils utils = new Utils();
-		utils.writeTimes(neo4jInsertionTimes, "data/neo4jInsertionTimes");
+//		utils.writeTimes(neo4jInsertionTimes, "data/neo4jInsertionTimes");
 		utils.deleteRecursively(new File(neo4jDBDir));
+		return neo4jInsertionTimes;
 	}
 
 }
