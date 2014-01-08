bd8aa18b1e87077d5f33519358b0b9ff96379e6c
Sotiris Beis <sot.beis@gmail.com>
2014-01-08 12:28:38 +0200

    Bug fixes

--- src/eu/socialsensor/main/Utils.java
+++ src/eu/socialsensor/main/Utils.java
@@ -9,30 +9,6 @@ import java.util.List;
 
 public class Utils {
 	
-	public static void main(String args[]) {
-		Utils utils = new Utils();
-		List<Double> list1 = new ArrayList<Double>();
-		list1.add(1.0);
-		list1.add(2.0);
-		list1.add(3.0);
-		list1.add(3.0);
-		List<Double> list2 = new ArrayList<Double>();
-		list2.add(4.0);
-		list2.add(5.0);
-		list2.add(6.0);
-		list2.add(6.0);
-		List<Double> list3 = new ArrayList<Double>();
-		list3.add(7.0);
-		list3.add(8.0);
-		list3.add(9.0);
-		list3.add(9.0);
-		List<List<Double>> data = new ArrayList<List<Double>>();
-		data.add(list1);
-		data.add(list2);
-		data.add(list3);
-		utils.calculateMeanList(data);
-	}
-	
 	/**
 	 * Calculates the mean value of x-dimenstion vector.
 	 * @param data - 2d ArrayList
@@ -41,10 +17,10 @@ public class Utils {
 		int yDim = data.size();
 		int xDim = data.get(0).size();
 		List<Double> meanData = new ArrayList<Double>();
-		for(int i = 0; i < yDim; i++) {
-			double[] temp = new double[xDim];
-			for(int j = 0; j < xDim; j++) {
-				temp[j] = data.get(i).get(j);
+		for(int i = 0; i < xDim; i++) {
+			double[] temp = new double[yDim];
+			for(int j = 0; j < yDim; j++) {
+				temp[j] = data.get(j).get(i);
 			}
 			meanData.add(calculateMean(temp));
 		}
