9b781db3c200faf89b7e581cfc844ee912efe6cf
Sotiris Beis <sot.beis@gmail.com>
2014-01-13 12:10:37 +0200

    Single insertion classes moved to insert package

---
+++ src/insert/Neo4jSingleInsertion.java
@@ -0,0 +1,141 @@
+package insert;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.index.Index;
+
+public class Neo4jSingleInsertion implements SingleInsertion {
+
+	
+	private GraphDatabaseService neo4jGraph = null;
+	private Index<Node> nodeIndex = null;
+	
+	private static enum RelTypes implements RelationshipType {
+	    SIMILAR
+	}
+	
+	public static void main(String args[]) {
+		Neo4jSingleInsertion test = new Neo4jSingleInsertion();
+		test.startup("data/neo4j");
+		test.createGraph("data/flickrEdges.txt");
+		test.shutdown();
+	}
+	
+	public void startup(String neo4jDBDir) {
+		System.out.println("The Neo4j database is now starting . . . .");
+		neo4jGraph = new GraphDatabaseFactory().newEmbeddedDatabase(neo4jDBDir);
+		nodeIndex = neo4jGraph.index().forNodes("nodes");
+	}
+	
+	public void shutdown() {
+		System.out.println("The Neo4j database is now shuting down . . . .");
+		if(neo4jGraph != null) {
+			neo4jGraph.shutdown();
+			nodeIndex = null;
+		}
+	}
+	
+	public List<Double> createGraph(String datasetDir) {
+		System.out.println("Incrementally creating the Neo4j database . . . .");
+		List<Double> insertionTimes = new ArrayList<Double>();
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(datasetDir)));
+			String line;
+			int nodesCounter = 0;
+			int lineCounter = 1;
+			int blocksCounter = 0;
+			int nodes = 0;
+			Transaction tx = null;
+			long start = System.currentTimeMillis();
+			long duration;
+			while((line = reader.readLine()) != null) {
+				if(lineCounter > 4) {
+					String[] parts = line.split("\t");
+					
+					Node srcNode = nodeIndex.get("nodeId", parts[0]).getSingle();
+					if(srcNode == null) {
+						tx = neo4jGraph.beginTx();
+						srcNode = neo4jGraph.createNode();
+						srcNode.setProperty("nodeId", parts[0]);
+						nodeIndex.add(srcNode, "nodeId", parts[0]);
+						tx.success();
+						tx.finish();
+						nodesCounter++;
+						nodes++;
+					}
+					
+					if(nodesCounter == 1000) {
+						duration = System.currentTimeMillis() - start;
+						insertionTimes.add((double) duration);
+						nodesCounter = 0;
+						start = System.currentTimeMillis();
+						
+						blocksCounter++;
+//						System.out.println("Nodes: "+nodes);
+//						System.out.println("Time: "+duration);
+//						System.out.println("blocks: "+blocksCounter);
+					}
+					
+					Node dstNode = nodeIndex.get("nodeId", parts[1]).getSingle();
+					if(dstNode == null) {
+						tx = neo4jGraph.beginTx();
+						dstNode = neo4jGraph.createNode();
+						dstNode.setProperty("nodeId", parts[1]);
+						nodeIndex.add(dstNode, "nodeId", parts[1]);
+						tx.success();
+						tx.finish();
+						nodesCounter++;
+						nodes++;
+					}
+					
+					tx = neo4jGraph.beginTx();
+					Relationship relationship = srcNode.createRelationshipTo(dstNode, RelTypes.SIMILAR);
+					tx.success();
+					tx.finish();
+					
+					if(nodesCounter == 1000) {
+						duration = System.currentTimeMillis() - start;
+						insertionTimes.add((double) duration);
+						nodesCounter = 0;
+						start = System.currentTimeMillis();
+						
+						blocksCounter++;
+//						System.out.println("Nodes: "+nodes);
+//						System.out.println("Time: "+duration);
+//						System.out.println("blocks: "+blocksCounter);
+					}
+				}
+				lineCounter++;
+			}
+			if(nodesCounter == 1000) {
+				duration = System.currentTimeMillis() - start;
+				insertionTimes.add((double) duration);
+				nodesCounter = 0;
+				start = System.currentTimeMillis();
+				
+				blocksCounter++;
+//				System.out.println("Nodes: "+nodes);
+//				System.out.println("Time: "+duration);
+//				System.out.println("blocks: "+blocksCounter);
+			}
+			
+			reader.close();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return insertionTimes;
+	}
+	
+}
---
+++ src/insert/OrientSingleInsertion.java
@@ -0,0 +1,132 @@
+package insert;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Index;
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientVertex;
+
+public class OrientSingleInsertion implements SingleInsertion {
+	
+	private OrientGraph orientGraph = null;
+	Index<OrientVertex> vetrices = null;
+	
+	public static void main(String args[]) {
+		
+	}
+	
+	public void startup(String orientDBDir) {
+		System.out.println("The Orient database is now starting . . . .");
+		orientGraph = new OrientGraph("local:"+orientDBDir);
+		orientGraph.createIndex("nodeId", OrientVertex.class);
+	    vetrices = orientGraph.getIndex("nodeId", OrientVertex.class);
+	}
+	
+	public void shutdown() {
+		System.out.println("The Orient database is now shuting down . . . .");
+		if(orientGraph != null) {
+			orientGraph.drop();
+			orientGraph.shutdown();
+			orientGraph = null;
+			vetrices = null;
+		}
+	}
+	
+	public List<Double> createGraph(String datasetDir) {
+		System.out.println("Incrementally creating the Orient database . . . .");
+		List<Double> insertionTimes = new ArrayList<Double>(); 
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(datasetDir)));
+			String line;
+			int lineCounter = 1;
+			int nodesCounter = 0;
+			OrientVertex srcVertex, dstVertex;
+			Iterable<OrientVertex> cache;
+			long start = System.currentTimeMillis();
+			long duration;
+			int blocksCounter = 0;
+			int nodes = 0;
+			while((line = reader.readLine()) != null) {
+				if(lineCounter > 4) {
+					String[] parts = line.split("\t");
+					
+					cache = vetrices.get("nodeId", parts[0]);
+					if(cache.iterator().hasNext()) {
+						srcVertex = cache.iterator().next();
+					}
+					else {
+						srcVertex = orientGraph.addVertex(null, "nodeId", parts[0] );
+						vetrices.put("nodeId", parts[0], srcVertex);
+						orientGraph.commit();
+						nodesCounter++;
+						nodes++;
+					}
+					
+					if(nodesCounter == 1000) {
+						duration = System.currentTimeMillis() - start;
+						insertionTimes.add((double) duration);
+						nodesCounter = 0;
+						start = System.currentTimeMillis();
+						
+						blocksCounter++;
+//						System.out.println("Nodes: "+nodes);
+//						System.out.println("Time: "+duration);
+//						System.out.println("blocks: "+blocksCounter);
+					}
+					
+					cache = vetrices.get("nodeId", parts[1]);
+					if(cache.iterator().hasNext()) {
+						dstVertex = cache.iterator().next();
+					}
+					else {
+						dstVertex = orientGraph.addVertex(null, "nodeId", parts[1] );
+						vetrices.put("nodeId", parts[1], dstVertex);
+						orientGraph.commit();
+						nodesCounter++;
+						nodes++;
+					}
+					
+					Edge edge = orientGraph.addEdge(null, srcVertex, dstVertex, "similar");
+					orientGraph.commit();
+					
+					if(nodesCounter == 1000) {
+						duration = System.currentTimeMillis() - start;
+						insertionTimes.add((double) duration);
+						nodesCounter = 0;
+						start = System.currentTimeMillis();
+						
+						blocksCounter++;
+//						System.out.println("Nodes: "+nodes);
+//						System.out.println("Time: "+duration);
+//						System.out.println("blocks: "+blocksCounter);
+					}
+				}
+				lineCounter++;
+			}			
+			
+			duration = System.currentTimeMillis() - start;
+			insertionTimes.add((double) duration);
+			blocksCounter++;
+//			System.out.println("Nodes: "+nodes);
+//			System.out.println("Time: "+duration);
+//			System.out.println("blocks: "+blocksCounter);
+			
+			reader.close();
+		}
+		catch(IOException ioe) {
+			System.out.println(ioe);
+			ioe.printStackTrace();
+		}
+		catch( Exception e ) {
+			System.out.println(e);
+			orientGraph.rollback();
+		}
+		return insertionTimes;
+	}
+}
