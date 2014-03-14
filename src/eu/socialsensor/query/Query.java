5e9382060ea8d6ed3d5dc1eed19c8d1dac301eb5
sotbeis <sarovios>
2014-03-14 15:54:47 +0200

    query interface added

---
+++ src/eu/socialsensor/query/Query.java
@@ -0,0 +1,11 @@
+package eu.socialsensor.query;
+
+public interface Query {
+	
+	public void findNeighborsOfAllNodes();
+	
+	public void findNodesOfAllEdges();
+	
+	public void findShortestPaths();
+	
+}
