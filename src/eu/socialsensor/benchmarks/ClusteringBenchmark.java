93fb88fa1e8f3d265fcfbb5ed1a8cb40cfe2ecf0
sotbeis <sarovios>
2014-03-14 11:42:23 +0200

    clustering benchmark added

--- src/eu/socialsensor/benchmarks/ClusteringBenchmark.java
+++ src/eu/socialsensor/benchmarks/ClusteringBenchmark.java
@@ -1,6 +1,13 @@
 package eu.socialsensor.benchmarks;
 
-import org.apache.log4j.Logger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import eu.socialsensor.clustering.LouvainMethodCache;
+import eu.socialsensor.graphdatabases.GraphDatabase;
+import eu.socialsensor.graphdatabases.TitanGraphDatabase;
 
 
 public class ClusteringBenchmark {
@@ -9,21 +16,91 @@ public class ClusteringBenchmark {
 	private final static String titanDBDir = "data/TitanDB";
 	private final static String neo4jDBDir = "data/Neo4jDB";
 	
-	private Logger logger = Logger.getLogger(ClusteringBenchmark.class);
+	private final List<Integer> enronCacheSizes;
 	
-	public void startBenchmark() {
+	public final static int CACHE_SIZE = 100;
+	
+	public final static boolean IS_RANDOMIZED = false;
+	
+	
+	public ClusteringBenchmark() {
 		
-		logger.info("Starting CLustering Benchmark");
-		
-		double[] orientTimes = new double[6];
-		double[] titanTimes = new double[6];
-		double[] neo4jTimes = new double[6];
-		
-		//Scenario 1
-		
+		enronCacheSizes = new ArrayList<Integer>(
+				Arrays.asList(1836, 3670, 5504, 7339, 9173, 11008, 12843));
 		
 	}
 	
+	public void startBenchmark() throws ExecutionException {
+		
+		System.out.println("###########################################################");
+		System.out.println("############## Starting Clustering Benchmark ##############");
+		System.out.println("###########################################################");
+		
+		for(int i = 0; i < enronCacheSizes.size(); i++) {
+			System.out.println("Cache size is set to "+enronCacheSizes.get(i));
+			double neo4jTime = neo4jClusteringBenchmark(enronCacheSizes.get(i));
+			System.out.println("GRAPHDATABASE: Neo4j");
+			System.out.println("DATASET: Enron");
+			System.out.println("CACHE: "+enronCacheSizes.get(i));
+			System.out.println("TIME: "+neo4jTime);
+			System.out.println("=======================================================");
+			System.out.println();
+			System.out.println();
+		}
+		
+		for(int i = 0; i < enronCacheSizes.size(); i++) {
+			System.out.println("Cache size is set to "+enronCacheSizes.get(i));
+			double orientTime = orientClusteringBenchmark(enronCacheSizes.get(i));
+			System.out.println("GRAPHDATABASE: OrientDB");
+			System.out.println("DATASET: Enron");
+			System.out.println("CACHE: "+enronCacheSizes.get(i));
+			System.out.println("TIME: "+orientTime);
+			System.out.println("=======================================================");
+			System.out.println();
+			System.out.println();
+		}
+		
+		for(int i = 0; i < enronCacheSizes.size(); i++) {
+			System.out.println("Cache size is set to "+enronCacheSizes.get(i));
+			double titanTime = titanClusteringBenchmark(enronCacheSizes.get(i));
+			System.out.println("GRAPHDATABASE: Neo4j");
+			System.out.println("DATASET: Enron");
+			System.out.println("CACHE: "+enronCacheSizes.get(i));
+			System.out.println("TIME: "+titanTime);
+			System.out.println("=======================================================");
+			System.out.println();
+			System.out.println();
+		}
+		
+	}
 	
+	public double titanClusteringBenchmark(int cacheSize) throws ExecutionException {
+		GraphDatabase titanGraphDatabase = new TitanGraphDatabase();
+		titanGraphDatabase.open(titanDBDir);
+		long start = System.currentTimeMillis();
+		LouvainMethodCache louvainMethodCache = new LouvainMethodCache(titanGraphDatabase, cacheSize, IS_RANDOMIZED);
+		louvainMethodCache.computeModularity();
+		long titanTime = System.currentTimeMillis() - start;
+		return titanTime/1000.0;
+	}
 	
+	public double orientClusteringBenchmark(int cacheSize) throws ExecutionException {
+		GraphDatabase orientGraphDatabase = new TitanGraphDatabase();
+		orientGraphDatabase.open(orientDBDir);
+		long start = System.currentTimeMillis();
+		LouvainMethodCache louvainMethodCache = new LouvainMethodCache(orientGraphDatabase, cacheSize, IS_RANDOMIZED);
+		louvainMethodCache.computeModularity();
+		long orientTime = System.currentTimeMillis() - start;
+		return orientTime/1000.0;
+	}
+	
+	public double neo4jClusteringBenchmark(int cacheSize) throws ExecutionException {
+		GraphDatabase neo4jGraphDatabase = new TitanGraphDatabase();
+		neo4jGraphDatabase.open(neo4jDBDir);
+		long start = System.currentTimeMillis();
+		LouvainMethodCache louvainMethodCache = new LouvainMethodCache(neo4jGraphDatabase, cacheSize, IS_RANDOMIZED);
+		louvainMethodCache.computeModularity();
+		long neo4jTime = System.currentTimeMillis() - start;
+		return neo4jTime /1000.0;
+	}
 }
--- src/eu/socialsensor/main/GraphDatabaseBenchmark.java
+++ src/eu/socialsensor/main/GraphDatabaseBenchmark.java
@@ -4,18 +4,21 @@ import com.thinkaurelius.titan.core.TitanFactory;
 import com.thinkaurelius.titan.core.TitanGraph;
 import com.tinkerpop.blueprints.Vertex;
 
+import eu.socialsensor.benchmarks.ClusteringBenchmark;
 import eu.socialsensor.benchmarks.FindNeighboursOfAllNodesBenchmark;
 import eu.socialsensor.benchmarks.MassiveInsertionBenchmark;
 import eu.socialsensor.benchmarks.SingleInsertionBenchmark;
 import eu.socialsensor.graphdatabases.GraphDatabase;
+import eu.socialsensor.graphdatabases.Neo4jGraphDatabase;
 import eu.socialsensor.graphdatabases.OrientGraphDatabase;
+import eu.socialsensor.graphdatabases.TitanGraphDatabase;
 
 public class GraphDatabaseBenchmark {
 		
-	private final static String ENRON_DATASET = "data/enronEdges.txt";
-	private final static String AMAZON_DATASET = "data/amazonEdges.txt";
-	private final static String YOUTUBE_DATASET = "data/youtubeEdges.txt";
-	private final static String LIVEJOURNAL_DATASET = "data/livejournalEdges.txt";
+	public final static String ENRON_DATASET = "data/enronEdges.txt";
+	public final static String AMAZON_DATASET = "data/amazonEdges.txt";
+	public final static String YOUTUBE_DATASET = "data/youtubeEdges.txt";
+	public final static String LIVEJOURNAL_DATASET = "data/livejournalEdges.txt";
 	
 	/**
 	 * This is the main function. Before you run the project un-comment
@@ -29,10 +32,13 @@ public class GraphDatabaseBenchmark {
 //		SingleInsertionBenchmark test = new SingleInsertionBenchmark(ENRON_DATASET);
 //		test.startBenchmark();
 		
-//		GraphDatabase titanGraphDatabase = new TitanGraphDatabase();
-//		titanGraphDatabase.createGraphForMassiveLoad("data/titan1");
-//		titanGraphDatabase.massiveModeLoading("data/network1.dat");
-//		titanGraphDatabase.shutdownMassiveGraph();
+//		ClusteringBenchmark clusteringBenchmark = new ClusteringBenchmark(ENRON_DATASET);
+//		clusteringBenchmark.startBenchmark();
+		
+		GraphDatabase titanGraphDatabase = new TitanGraphDatabase();
+		titanGraphDatabase.createGraphForMassiveLoad("data/titan");
+		titanGraphDatabase.massiveModeLoading("data/network.dat");
+		titanGraphDatabase.shutdownMassiveGraph();
 		
 //		titanGraphDatabase.createGraphForSingleLoad("data/titan");
 //		titanGraphDatabase.singleModeLoading(ENRON_DATASET);
@@ -46,18 +52,18 @@ public class GraphDatabaseBenchmark {
 		orientGraphDatabase.massiveModeLoading("data/network.dat");
 		orientGraphDatabase.shutdownMassiveGraph();
 		
-//		orientGraphDatabase.createGraphForSingleLoad("data/orient");
-//		orientGraphDatabase.singleModeLoading(ENRON_DATASET);
+//		orientGraphDatabase.createGraphForSingleLoad("data/orient1");
+//		orientGraphDatabase.singleModeLoading("data/network.dat");
 //		orientGraphDatabase.shutdown();
 //		
 //		orientGraphDatabase.open("data/orient");
 
 		
-		
-//		GraphDatabase neo4jGraphDatabase = new Neo4jGraphDatabase();
-//		neo4jGraphDatabase.createGraphForMassiveLoad("data/neo4j");
-//		neo4jGraphDatabase.massiveModeLoading(ENRON_DATASET);
-//		neo4jGraphDatabase.shutdownMassiveGraph();
+//		
+		GraphDatabase neo4jGraphDatabase = new Neo4jGraphDatabase();
+		neo4jGraphDatabase.createGraphForMassiveLoad("data/neo4j");
+		neo4jGraphDatabase.massiveModeLoading("data/network.dat");
+		neo4jGraphDatabase.shutdownMassiveGraph();
 		
 //		neo4jGraphDatabase.createGraphForSingleLoad("data/neo4j");
 //		neo4jGraphDatabase.singleModeLoading(ENRON_DATASET);
