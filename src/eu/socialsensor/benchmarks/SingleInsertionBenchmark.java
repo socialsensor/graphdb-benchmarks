66829934d4f398b9d6c0ac91f46ffeb76f3ad536
sotbeis <sot.beis@gmail.com>
2014-08-27 11:44:50 +0300

    bug fixes + trash removal

--- src/eu/socialsensor/benchmarks/SingleInsertionBenchmark.java
+++ src/eu/socialsensor/benchmarks/SingleInsertionBenchmark.java
@@ -15,6 +15,7 @@ import eu.socialsensor.graphdatabases.SparkseeGraphDatabase;
 import eu.socialsensor.graphdatabases.TitanGraphDatabase;
 import eu.socialsensor.insert.Neo4jSingleInsertion;
 import eu.socialsensor.insert.OrientSingleInsertion;
+import eu.socialsensor.insert.SparkseeSingleInsertion;
 import eu.socialsensor.insert.TitanSingleInsertion;
 import eu.socialsensor.main.GraphDatabaseBenchmark;
 import eu.socialsensor.utils.PermuteMethod;
@@ -71,49 +72,6 @@ public class SingleInsertionBenchmark implements Benchmark {
 			}
 		}
 		
-		//Scenario 1
-//		System.out.println("");
-//		logger.info("Scenario 1");
-//		sparkseeSinglesInsertionBenchmark();
-//		titanSingleInsertionBenchmark();
-//		orientSingleInsertionBenchmark();
-//		neo4jSinglesInsertionBenchmark();
-
-		//Scenario 2
-//		System.out.println("");
-//		logger.info("Scenario 2");
-//		titanSingleInsertionBenchmark();
-//		neo4jSinglesInsertionBenchmark();
-//		orientSingleInsertionBenchmark();
-//
-//		//Scenario 3
-//		System.out.println("");
-//		logger.info("Scenario 3");
-//		orientSingleInsertionBenchmark();
-//		titanSingleInsertionBenchmark();
-//		neo4jSinglesInsertionBenchmark();
-//
-//		//Scenario 4
-//		System.out.println("");
-//		logger.info("Scenario 4");
-//		orientSingleInsertionBenchmark();
-//		neo4jSinglesInsertionBenchmark();
-//		titanSingleInsertionBenchmark();
-//
-//		//Scenario 5
-//		System.out.println("");
-//		logger.info("Scenario 5");
-//		neo4jSinglesInsertionBenchmark();
-//		titanSingleInsertionBenchmark();
-//		orientSingleInsertionBenchmark();
-//
-//		//Scenario 6
-//		System.out.println("");
-//		logger.info("Scenario 6");
-//		neo4jSinglesInsertionBenchmark();
-//		orientSingleInsertionBenchmark();
-//		titanSingleInsertionBenchmark();
-		
 		System.out.println("");
 		logger.info("Single Insertion Benchmark finished");
 		
@@ -123,24 +81,30 @@ public class SingleInsertionBenchmark implements Benchmark {
 		utils.getDocumentsAs2dList(orientInsertionTimesOfEachScenario, OrientSingleInsertion.INSERTION_TIMES_OUTPUT_PATH);
 		List<List<Double>> neo4jInsertionTimesOfEachScenario = new ArrayList<List<Double>>(SingleInsertionBenchmark.SCENARIOS);
 		utils.getDocumentsAs2dList(neo4jInsertionTimesOfEachScenario, Neo4jSingleInsertion.INSERTION_TIMES_OUTPUT_PATH);
+		List<List<Double>> sparkseeInsertionTimesOfEachScenario = new ArrayList<List<Double>>(SingleInsertionBenchmark.SCENARIOS);
+		utils.getDocumentsAs2dList(sparkseeInsertionTimesOfEachScenario, SparkseeSingleInsertion.INSERTION_TIMES_OUTPUT_PATH);
 		
 		//compute mean time
 		List<Double> titanMeanInsertionTimes = utils.calculateMeanList(titanInsertionTimesOfEachScenario);
 		List<Double> orientMeanInsertionTimes = utils.calculateMeanList(orientInsertionTimesOfEachScenario);
 		List<Double> neo4jMeanInsertionTimes = utils.calculateMeanList(neo4jInsertionTimesOfEachScenario);
+		List<Double> sparkseeMeanInsertionTimes = utils.calculateMeanList(sparkseeInsertionTimesOfEachScenario);
 		
 		System.out.println("");
 		logger.info("Write results to "+resultsFolder);
 		utils.writeTimes(titanMeanInsertionTimes, TitanSingleInsertion.INSERTION_TIMES_OUTPUT_PATH);
 		utils.writeTimes(orientMeanInsertionTimes, OrientSingleInsertion.INSERTION_TIMES_OUTPUT_PATH);
 		utils.writeTimes(neo4jMeanInsertionTimes, Neo4jSingleInsertion.INSERTION_TIMES_OUTPUT_PATH);
+		utils.writeTimes(sparkseeMeanInsertionTimes, SparkseeSingleInsertion.INSERTION_TIMES_OUTPUT_PATH);
 		
 		//clearing thrash
 		utils.deleteMultipleFiles(TitanSingleInsertion.INSERTION_TIMES_OUTPUT_PATH, SingleInsertionBenchmark.SCENARIOS);
 		utils.deleteMultipleFiles(OrientSingleInsertion.INSERTION_TIMES_OUTPUT_PATH, SingleInsertionBenchmark.SCENARIOS);
 		utils.deleteMultipleFiles(Neo4jSingleInsertion.INSERTION_TIMES_OUTPUT_PATH, SingleInsertionBenchmark.SCENARIOS);
+		utils.deleteMultipleFiles(SparkseeSingleInsertion.INSERTION_TIMES_OUTPUT_PATH, SingleInsertionBenchmark.SCENARIOS);
 	}
 	
+	@SuppressWarnings("unused")
 	private void titanSingleInsertionBenchmark() {
 		GraphDatabase titanGraphDatabase = new TitanGraphDatabase();
 		titanGraphDatabase.createGraphForSingleLoad(GraphDatabaseBenchmark.TITANDB_PATH);
@@ -149,6 +113,7 @@ public class SingleInsertionBenchmark implements Benchmark {
 		titanGraphDatabase.delete(GraphDatabaseBenchmark.TITANDB_PATH);
 	}
 		
+	@SuppressWarnings("unused")
 	private void orientSingleInsertionBenchmark() {
 		GraphDatabase orientGraphDatabase = new OrientGraphDatabase();
 		orientGraphDatabase.createGraphForSingleLoad(GraphDatabaseBenchmark.ORIENTDB_PATH);
@@ -157,6 +122,7 @@ public class SingleInsertionBenchmark implements Benchmark {
 		orientGraphDatabase.delete(GraphDatabaseBenchmark.ORIENTDB_PATH);
 	}
 	
+	@SuppressWarnings("unused")
 	private void neo4jSingleInsertionBenchmark() {
 		GraphDatabase neo4jGraphDatabase = new Neo4jGraphDatabase();
 		neo4jGraphDatabase.createGraphForSingleLoad(GraphDatabaseBenchmark.NEO4JDB_PATH);
@@ -165,6 +131,7 @@ public class SingleInsertionBenchmark implements Benchmark {
 		neo4jGraphDatabase.delete(GraphDatabaseBenchmark.NEO4JDB_PATH);
 	}
 	
+	@SuppressWarnings("unused")
 	private void sparkseeSingleInsertionBenchmark() {
 		GraphDatabase sparkseeGraphDatabase = new SparkseeGraphDatabase();
 		sparkseeGraphDatabase.createGraphForSingleLoad(GraphDatabaseBenchmark.SPARKSEEDB_PATH);
