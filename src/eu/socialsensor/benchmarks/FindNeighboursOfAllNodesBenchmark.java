dbdd1023635f7ccd330b1306434a431a22b606b6
sotbeis <sot.beis@gmail.com>
2014-08-28 15:50:21 +0300

    FindNeighborsOfAllNodes benchmark added

--- src/eu/socialsensor/benchmarks/FindNeighboursOfAllNodesBenchmark.java
+++ src/eu/socialsensor/benchmarks/FindNeighboursOfAllNodesBenchmark.java
@@ -3,6 +3,8 @@ package eu.socialsensor.benchmarks;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -10,8 +12,10 @@ import org.apache.log4j.Logger;
 import eu.socialsensor.graphdatabases.GraphDatabase;
 import eu.socialsensor.graphdatabases.Neo4jGraphDatabase;
 import eu.socialsensor.graphdatabases.OrientGraphDatabase;
+import eu.socialsensor.graphdatabases.SparkseeGraphDatabase;
 import eu.socialsensor.graphdatabases.TitanGraphDatabase;
 import eu.socialsensor.main.GraphDatabaseBenchmark;
+import eu.socialsensor.utils.PermuteMethod;
 import eu.socialsensor.utils.Utils;
 
 /**
@@ -19,67 +23,70 @@ import eu.socialsensor.utils.Utils;
  * @author sotbeis
  * @email sotbeis@iti.gr
  */
+
 public class FindNeighboursOfAllNodesBenchmark implements Benchmark {
 	
+	private static int SCENARIOS = 24;
 	private final String resultFile = "QW-FNResults.txt";
 	
 	private Logger logger = Logger.getLogger(FindNeighboursOfAllNodesBenchmark.class);
 	
+	private double[] orientTimes = new double[SCENARIOS];
+	private double[] titanTimes = new double[SCENARIOS];
+	private double[] neo4jTimes = new double[SCENARIOS];
+	private double[] sparkseeTimes = new double[SCENARIOS];
+	
+	private int titanScenarioCount = 0;
+	private int orientScenarioCount = 0;
+	private int neo4jScenarioCount = 0;
+	private int sparkseeScenarioCount = 0;
+
 	@Override
 	public void startBenchmark() {
 		logger.setLevel(Level.INFO);
+		System.out.println("");
 		logger.info("Executing Find Neighbours of All Nodes Benchmark . . . .");
 		
-		double[] orientTimes = new double[6];
-		double[] titanTimes = new double[6];
-		double[] neo4jTimes = new double[6];
-		
-		logger.info("Scenario 1");
-		orientTimes[0] = orientFindNeighboursOfAllNodesBenchmark();
-		titanTimes[0] = titanFindNeighboursOfAllNodesBenchmark();
-		neo4jTimes[0] = neo4jFindNeighboursOfAllNodesBenchmark();
-		
-		logger.info("Scenario 2");
-		orientTimes[1] = orientFindNeighboursOfAllNodesBenchmark();
-		neo4jTimes[1] = neo4jFindNeighboursOfAllNodesBenchmark();
-		titanTimes[1] = titanFindNeighboursOfAllNodesBenchmark();
-		
-		logger.info("Scenario 3");
-		neo4jTimes[2] = neo4jFindNeighboursOfAllNodesBenchmark();
-		orientTimes[2] = orientFindNeighboursOfAllNodesBenchmark();
-		titanTimes[2] = titanFindNeighboursOfAllNodesBenchmark();
-		
-		logger.info("Scenario 4");
-		neo4jTimes[3] = neo4jFindNeighboursOfAllNodesBenchmark();
-		titanTimes[3] = titanFindNeighboursOfAllNodesBenchmark();
-		orientTimes[3] = orientFindNeighboursOfAllNodesBenchmark();
-		
-		logger.info("Scenario 5");
-		titanTimes[4] = titanFindNeighboursOfAllNodesBenchmark();
-		neo4jTimes[4] = neo4jFindNeighboursOfAllNodesBenchmark();
-		orientTimes[4] = orientFindNeighboursOfAllNodesBenchmark();
-		
-		logger.info("Scenario 6");
-		titanTimes[5] = titanFindNeighboursOfAllNodesBenchmark();
-		orientTimes[5] = orientFindNeighboursOfAllNodesBenchmark();
-		neo4jTimes[5] = neo4jFindNeighboursOfAllNodesBenchmark();
-		
-		logger.info("Find Neighbours of All Nodes Benchmark finished");
-		
 		Utils utils = new Utils();
+		Class<FindNeighboursOfAllNodesBenchmark> c = FindNeighboursOfAllNodesBenchmark.class;
+		Method[] methods = utils.filter(c.getDeclaredMethods(), "FindNeighboursOfAllNodesBenchmark");
+		PermuteMethod permutations = new PermuteMethod(methods);
+		int cntPermutations = 1;
+		while(permutations.hasNext()) {
+			logger.info("Scenario " + cntPermutations++);
+			for(Method permutation : permutations.next()) {
+				try {
+					permutation.invoke(this, null);
+				} catch (IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
 		
+		System.out.println("");
+		logger.info("Find Neighbours of All Nodes Benchmark finished");
+				
 		double meanOrientTime = utils.calculateMean(orientTimes);
 		double meanTitanTime = utils.calculateMean(titanTimes);
 		double meanNeo4jTime = utils.calculateMean(neo4jTimes);
+		double meanSparkseeTime = utils.calculateMean(sparkseeTimes);
+		
 		double varOrientTime = utils.calculateVariance(meanOrientTime, orientTimes);
 		double varTitanTime = utils.calculateVariance(meanTitanTime, titanTimes);
 		double varNeo4jTime = utils.calculateVariance(meanNeo4jTime, neo4jTimes);
+		double varSparkseeTime = utils.calculateVariance(meanSparkseeTime, sparkseeTimes);
+		
 		double stdOrientTime = utils.calculateStdDeviation(varOrientTime);
 		double stdTitanTime = utils.calculateStdDeviation(varTitanTime);
 		double stdNeo4jTime = utils.calculateStdDeviation(varNeo4jTime);
+		double stdSparkseeTime = utils.calculateStdDeviation(varSparkseeTime);
 		
 		String resultsFolder = GraphDatabaseBenchmark.inputPropertiesFile.getProperty("RESULTS_PATH");
 		String output = resultsFolder+resultFile;
+		System.out.println("");
 		logger.info("Write results to "+output);
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(output));
@@ -109,6 +116,15 @@ public class FindNeighboursOfAllNodesBenchmark implements Benchmark {
 			out.write("Mean Value: "+meanNeo4jTime);
 			out.write("\n");
 			out.write("STD Value: "+stdNeo4jTime);
+			out.write("\n");
+			out.write("\n");
+			out.write("Sparksee execution time");
+			out.write("\n");
+			out.write("Mean Value: " + meanSparkseeTime);
+			out.write("\n");
+			out.write("STD Value: " + stdSparkseeTime);
+			out.write("\n");
+			out.write("########################################################");
 			
 			out.flush();
 			out.close();
@@ -119,34 +135,56 @@ public class FindNeighboursOfAllNodesBenchmark implements Benchmark {
 		
 	}
 	
-	private double orientFindNeighboursOfAllNodesBenchmark() {
+	@SuppressWarnings("unused")
+	private void orientFindNeighboursOfAllNodesBenchmark() {
 		GraphDatabase orientGraphDatabase = new OrientGraphDatabase();
 		orientGraphDatabase.open(GraphDatabaseBenchmark.ORIENTDB_PATH);
 		long start = System.currentTimeMillis();
 		orientGraphDatabase.neighborsOfAllNodesQuery();
 		long orientTime = System.currentTimeMillis() - start;
 		orientGraphDatabase.shutdown();
-		return orientTime/1000.0;
+		orientTimes[orientScenarioCount] = orientTime / 1000.0;
+		orientScenarioCount++;
+		System.out.println("Orient " + orientScenarioCount + ": " + orientTime );
 	}
 	
-	private double titanFindNeighboursOfAllNodesBenchmark() {
+	@SuppressWarnings("unused")
+	private void titanFindNeighboursOfAllNodesBenchmark() {
 		GraphDatabase titanGraphDatabase = new TitanGraphDatabase();
 		titanGraphDatabase.open(GraphDatabaseBenchmark.TITANDB_PATH);
 		long start = System.currentTimeMillis();
 		titanGraphDatabase.neighborsOfAllNodesQuery();
 		long titanTime = System.currentTimeMillis() - start;
 		titanGraphDatabase.shutdown();
-		return titanTime/1000.0;
+		titanTimes[titanScenarioCount] = titanTime / 1000.0;
+		titanScenarioCount++;
+		System.out.println("Titan " + titanScenarioCount + ": " + titanTime );
 	}
 	
-	private double neo4jFindNeighboursOfAllNodesBenchmark() {
+	@SuppressWarnings("unused")
+	private void neo4jFindNeighboursOfAllNodesBenchmark() {
 		GraphDatabase neo4jGraphDatabase = new Neo4jGraphDatabase();
 		neo4jGraphDatabase.open(GraphDatabaseBenchmark.NEO4JDB_PATH);
 		long start = System.currentTimeMillis();
 		neo4jGraphDatabase.neighborsOfAllNodesQuery();
 		long neo4jTime = System.currentTimeMillis() - start;
 		neo4jGraphDatabase.shutdown();
-		return neo4jTime/1000.0;
+		neo4jTimes[neo4jScenarioCount] = neo4jTime / 1000.0;
+		neo4jScenarioCount++;
+		System.out.println("Neo4j " + neo4jScenarioCount + ": " + neo4jTime );
+	}
+	
+	@SuppressWarnings("unused")
+	private void sparkseeFindNeighboursOfAllNodesBenchmark() {
+		GraphDatabase sparkseeGraphDatabase = new SparkseeGraphDatabase();
+		sparkseeGraphDatabase.open(GraphDatabaseBenchmark.SPARKSEEDB_PATH);
+		long start = System.currentTimeMillis();
+		sparkseeGraphDatabase.neighborsOfAllNodesQuery();
+		long sparkseeTime = System.currentTimeMillis() - start;
+		sparkseeGraphDatabase.shutdown();
+		sparkseeTimes[sparkseeScenarioCount] = sparkseeTime / 1000.0;
+		sparkseeScenarioCount++;
+		System.out.println("Sparksee " + sparkseeScenarioCount + ": " + sparkseeTime);
 	}
 
 }
