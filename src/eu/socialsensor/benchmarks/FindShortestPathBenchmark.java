ae86b66039555af76214238b1f49aa80cb373e51
sotbeis <sot.beis@gmail.com>
2014-08-28 17:15:42 +0300

    sparksee shortest path benchmark added

--- src/eu/socialsensor/benchmarks/FindShortestPathBenchmark.java
+++ src/eu/socialsensor/benchmarks/FindShortestPathBenchmark.java
@@ -3,6 +3,8 @@ package eu.socialsensor.benchmarks;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -13,8 +15,10 @@ import org.apache.log4j.Logger;
 import eu.socialsensor.graphdatabases.GraphDatabase;
 import eu.socialsensor.graphdatabases.Neo4jGraphDatabase;
 import eu.socialsensor.graphdatabases.OrientGraphDatabase;
+import eu.socialsensor.graphdatabases.SparkseeGraphDatabase;
 import eu.socialsensor.graphdatabases.TitanGraphDatabase;
 import eu.socialsensor.main.GraphDatabaseBenchmark;
+import eu.socialsensor.utils.PermuteMethod;
 import eu.socialsensor.utils.Utils;
 
 /**
@@ -23,12 +27,23 @@ import eu.socialsensor.utils.Utils;
  * @email sotbeis@iti.gr
  */
 public class FindShortestPathBenchmark implements Benchmark {
-		
+	
+	private static int SCENARIOS = 24;
 	public static Set<Integer> generatedNodes;
 	
 	private final String resultFile = "QW-FSResults.txt";
 	
 	private Logger logger = Logger.getLogger(FindShortestPathBenchmark.class);
+	
+	private double[] orientTimes = new double[SCENARIOS];
+	private double[] titanTimes = new double[SCENARIOS];
+	private double[] neo4jTimes = new double[SCENARIOS];
+	private double[] sparkseeTimes = new double[SCENARIOS];
+	
+	private int titanScenarioCount = 0;
+	private int orientScenarioCount = 0;
+	private int neo4jScenarioCount = 0;
+	private int sparkseeScenarioCount = 0;
 	
 	@Override
 	public void startBenchmark() {
@@ -46,54 +61,45 @@ public class FindShortestPathBenchmark implements Benchmark {
 			generatedNodes.add(randomNum);
 		}
 		
-		double[] orientTimes = new double[6];
-		double[] titanTimes = new double[6];
-		double[] neo4jTimes = new double[6];
-		
-		logger.info("Scenario 1");
-		orientTimes[0] = orientFindShortestPathBenchmark();
-		titanTimes[0] = titanFindShortestPathBenchmark();
-		neo4jTimes[0] = neo4jFindShortestPathBenchmark();
-		
-		logger.info("Scenario 2");
-		orientTimes[1] = orientFindShortestPathBenchmark();
-		neo4jTimes[1] = neo4jFindShortestPathBenchmark();
-		titanTimes[1] = titanFindShortestPathBenchmark();
-		
-		logger.info("Scenario 3");
-		neo4jTimes[2] = neo4jFindShortestPathBenchmark();
-		orientTimes[2] = orientFindShortestPathBenchmark();
-		titanTimes[2] = titanFindShortestPathBenchmark();
-		
-		logger.info("Scenario 4");
-		neo4jTimes[3] = neo4jFindShortestPathBenchmark();
-		titanTimes[3] = titanFindShortestPathBenchmark();
-		orientTimes[3] = orientFindShortestPathBenchmark();
-		
-		logger.info("Scenario 5");
-		titanTimes[4] = titanFindShortestPathBenchmark();
-		neo4jTimes[4] = neo4jFindShortestPathBenchmark();
-		orientTimes[4] = orientFindShortestPathBenchmark();
-		
-		logger.info("Scenario 6");
-		titanTimes[5] = titanFindShortestPathBenchmark();
-		orientTimes[5] = orientFindShortestPathBenchmark();
-		neo4jTimes[5] = neo4jFindShortestPathBenchmark();
-		
 		Utils utils = new Utils();
+		Class<FindShortestPathBenchmark> c = FindShortestPathBenchmark.class;
+		Method[] methods = utils.filter(c.getDeclaredMethods(), "FindShortestPathBenchmark");
+		PermuteMethod permutations = new PermuteMethod(methods);
+		int cntPermutations = 1;
+		while(permutations.hasNext()) {
+			logger.info("Scenario " + cntPermutations++);
+			for(Method permutation : permutations.next()) {
+				try {
+					permutation.invoke(this, null);
+				} catch (IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		
+		System.out.println("");
+		logger.info("Find Shortest Path Benchmark finished");
 		
 		double meanOrientTime = utils.calculateMean(orientTimes);
 		double meanTitanTime = utils.calculateMean(titanTimes);
 		double meanNeo4jTime = utils.calculateMean(neo4jTimes);
+		double meanSparkseeTime = utils.calculateMean(sparkseeTimes);
+		
 		double varOrientTime = utils.calculateVariance(meanOrientTime, orientTimes);
 		double varTitanTime = utils.calculateVariance(meanTitanTime, titanTimes);
 		double varNeo4jTime = utils.calculateVariance(meanNeo4jTime, neo4jTimes);
+		double varSparkseeTime = utils.calculateVariance(meanSparkseeTime, sparkseeTimes);
+		
 		double stdOrientTime = utils.calculateStdDeviation(varOrientTime);
 		double stdTitanTime = utils.calculateStdDeviation(varTitanTime);
 		double stdNeo4jTime = utils.calculateStdDeviation(varNeo4jTime);
+		double stdSparkseeTime = utils.calculateStdDeviation(varSparkseeTime);
 		
 		String resultsFolder = GraphDatabaseBenchmark.inputPropertiesFile.getProperty("RESULTS_PATH");
 		String output = resultsFolder+resultFile;
+		System.out.println("");
 		logger.info("Write results to "+output);
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(output));
@@ -123,6 +129,15 @@ public class FindShortestPathBenchmark implements Benchmark {
 			out.write("Mean Value: "+meanNeo4jTime);
 			out.write("\n");
 			out.write("STD Value: "+stdNeo4jTime);
+			out.write("\n");
+			out.write("\n");
+			out.write("Sparksee execution time");
+			out.write("\n");
+			out.write("Mean Value: " + meanSparkseeTime);
+			out.write("\n");
+			out.write("STD Value: " + stdSparkseeTime);
+			out.write("\n");
+			out.write("########################################################");
 			
 			out.flush();
 			out.close();
@@ -133,33 +148,52 @@ public class FindShortestPathBenchmark implements Benchmark {
 		
 	}
 	
-	private double orientFindShortestPathBenchmark() {
+	@SuppressWarnings("unused")
+	private void orientFindShortestPathBenchmark() {
 		GraphDatabase orientGraphDatabase = new OrientGraphDatabase();
 		orientGraphDatabase.open(GraphDatabaseBenchmark.ORIENTDB_PATH);
 		long start = System.currentTimeMillis();
 		orientGraphDatabase.shorestPathQuery();
 		long orientTime = System.currentTimeMillis() - start;
 		orientGraphDatabase.shutdown();
-		return orientTime/1000.0;	
+		orientTimes[orientScenarioCount] = orientTime / 1000.0;
+		orientScenarioCount++;
 	}
 	
-	private double titanFindShortestPathBenchmark() {
+	@SuppressWarnings("unused")
+	private void titanFindShortestPathBenchmark() {
 		GraphDatabase titanGraphDatabase = new TitanGraphDatabase();
 		titanGraphDatabase.open(GraphDatabaseBenchmark.TITANDB_PATH);
 		long start = System.currentTimeMillis();
 		titanGraphDatabase.shorestPathQuery();
 		long titanTime = System.currentTimeMillis() - start;
 		titanGraphDatabase.shutdown();
-		return titanTime/1000.0;
+		titanTimes[titanScenarioCount] = titanTime / 1000.0;
+		titanScenarioCount++;
 	}
 	
-	private double neo4jFindShortestPathBenchmark() {
+	@SuppressWarnings("unused")
+	private void neo4jFindShortestPathBenchmark() {
 		GraphDatabase neo4jGraphDatabase = new Neo4jGraphDatabase();
 		neo4jGraphDatabase.open(GraphDatabaseBenchmark.NEO4JDB_PATH);
 		long start = System.currentTimeMillis();
 		neo4jGraphDatabase.shorestPathQuery();
 		long neo4jTime = System.currentTimeMillis() - start;
 		neo4jGraphDatabase.shutdown();
-		return neo4jTime/1000.0;	}
+		neo4jTimes[neo4jScenarioCount] = neo4jTime / 1000.0;
+		neo4jScenarioCount++;
+	}
+	
+	@SuppressWarnings("unused")
+	private void sparkseeFindShortestPathBenchmark() {
+		GraphDatabase sparkseeGraphDatabase = new SparkseeGraphDatabase();
+		sparkseeGraphDatabase.open(GraphDatabaseBenchmark.SPARKSEEDB_PATH);
+		long start = System.currentTimeMillis();
+		sparkseeGraphDatabase.shorestPathQuery();
+		long sparkseeTime = System.currentTimeMillis() - start;
+		sparkseeGraphDatabase.shutdown();
+		sparkseeTimes[sparkseeScenarioCount] = sparkseeTime / 1000.0;
+		sparkseeScenarioCount++;
+	}
 
 }
