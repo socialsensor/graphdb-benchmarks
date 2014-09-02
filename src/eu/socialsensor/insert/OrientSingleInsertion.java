d4d90d9f6612339351eab77cd71eb93b453581a1
lvca <l.garulli@gmail.com>
2014-09-02 22:09:43 +0200

    Fixed OrientDB implementation

--- src/eu/socialsensor/insert/OrientSingleInsertion.java
+++ src/eu/socialsensor/insert/OrientSingleInsertion.java
@@ -1,5 +1,12 @@
 package eu.socialsensor.insert;
 
+import com.orientechnologies.orient.core.index.OIndex;
+import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientVertex;
+import eu.socialsensor.benchmarks.SingleInsertionBenchmark;
+import eu.socialsensor.utils.Utils;
+import org.apache.log4j.Level;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,116 +14,94 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
-import com.tinkerpop.blueprints.Index;
-import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.orient.OrientGraph;
-import com.tinkerpop.blueprints.impls.orient.OrientVertex;
-
-import eu.socialsensor.benchmarks.SingleInsertionBenchmark;
-import eu.socialsensor.utils.Utils;
-
 /**
- * Implementation of single Insertion in OrientDB
- * graph database
+ * Implementation of single Insertion in OrientDB graph database
  * 
  * @author sotbeis
  * @email sotbeis@iti.gr
  * 
  */
-public class OrientSingleInsertion implements Insertion {
-	
-	public static String INSERTION_TIMES_OUTPUT_PATH = null;
+public class OrientSingleInsertion extends OrientAbstractInsertion {
 
-	private static int count;
-	
-	private OrientGraph orientGraph = null;
-	Index<Vertex> vetrices = null;
-	
-	private Logger logger = Logger.getLogger(OrientSingleInsertion.class);
-	
-	public OrientSingleInsertion(OrientGraph orientGraph, Index<Vertex> vertices) {
-		this.orientGraph = orientGraph;
-		this.vetrices = vertices;
-	}
-	
-	@Override
-	public void createGraph(String datasetDir) {
-		INSERTION_TIMES_OUTPUT_PATH = SingleInsertionBenchmark.INSERTION_TIMES_OUTPUT_PATH + ".orient";
-		logger.setLevel(Level.INFO);
-		count++;
-		logger.info("Incrementally loading data in Orient database . . . .");
-		List<Double> insertionTimes = new ArrayList<Double>(); 
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(datasetDir)));
-			String line;
-			int lineCounter = 1;
-			int nodesCounter = 0;
-			Vertex srcVertex, dstVertex;
-			Iterable<Vertex> cache;
-			long start = System.currentTimeMillis();
-			long duration;
-			while((line = reader.readLine()) != null) {
-				if(lineCounter > 4) {
-					String[] parts = line.split("\t");
-					
-					cache = vetrices.get("nodeId", parts[0]);
-					if(cache.iterator().hasNext()) {
-						srcVertex = cache.iterator().next();
-					}
-					else {
-						srcVertex = orientGraph.addVertex(null, "nodeId", parts[0] );
-						vetrices.put("nodeId", parts[0], srcVertex);
-						orientGraph.commit();
-						nodesCounter++;
-					}
-					
-					if(nodesCounter == 1000) {
-						duration = System.currentTimeMillis() - start;
-						insertionTimes.add((double) duration);
-						nodesCounter = 0;
-						start = System.currentTimeMillis();
-					}
-					
-					cache = vetrices.get("nodeId", parts[1]);
-					if(cache.iterator().hasNext()) {
-						dstVertex = cache.iterator().next();
-					}
-					else {
-						dstVertex = orientGraph.addVertex(null, "nodeId", parts[1] );
-						vetrices.put("nodeId", parts[1], dstVertex);
-						orientGraph.commit();
-						nodesCounter++;
-					}
-					
-					orientGraph.addEdge(null, srcVertex, dstVertex, "similar");
-					orientGraph.commit();
-					
-					if(nodesCounter == 1000) {
-						duration = System.currentTimeMillis() - start;
-						insertionTimes.add((double) duration);
-						nodesCounter = 0;
-						start = System.currentTimeMillis();
-					}
-				}
-				lineCounter++;
-			}			
-			
-			duration = System.currentTimeMillis() - start;
-			insertionTimes.add((double) duration);
-			reader.close();
-		}
-		catch(IOException ioe) {
-			System.out.println(ioe);
-			ioe.printStackTrace();
-		}
-		catch( Exception e ) {
-			System.out.println(e);
-			orientGraph.rollback();
-		}
-		Utils utils = new Utils();
-		utils.writeTimes(insertionTimes, OrientSingleInsertion.INSERTION_TIMES_OUTPUT_PATH+"."+count);
-	}
+  public OrientSingleInsertion(OrientBaseGraph orientGraph, OIndex vertices) {
+    super(orientGraph, vertices);
+  }
+
+  @Override
+  public void createGraph(String datasetDir) {
+    INSERTION_TIMES_OUTPUT_PATH = SingleInsertionBenchmark.INSERTION_TIMES_OUTPUT_PATH + ".orient";
+    logger.setLevel(Level.INFO);
+    count++;
+    logger.info("Incrementally loading data in Orient database . . . .");
+    List<Double> insertionTimes = new ArrayList<Double>();
+    try {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(datasetDir)));
+      String line;
+      int lineCounter = 1;
+      int nodesCounter = 0;
+      OrientVertex srcVertex, dstVertex;
+      Iterable<OrientVertex> cache;
+
+      long start = System.currentTimeMillis();
+      long duration;
+      while ((line = reader.readLine()) != null) {
+        if (lineCounter > 4) {
+          String[] parts = line.split("\t");
+
+          final Integer key = Integer.parseInt(parts[0]);
+          cache = vertexIndexLookup(key);
+          if (cache.iterator().hasNext()) {
+            srcVertex = cache.iterator().next();
+          } else {
+            srcVertex = orientGraph.addVertex(null, "nodeId", key);
+            vertices.put(key, srcVertex);
+            orientGraph.commit();
+            nodesCounter++;
+          }
+
+          if (nodesCounter == 1000) {
+            duration = System.currentTimeMillis() - start;
+            insertionTimes.add((double) duration);
+            nodesCounter = 0;
+            start = System.currentTimeMillis();
+          }
+
+          final Integer key2 = Integer.parseInt(parts[0]);
+
+          cache = vertexIndexLookup(key2);
+          if (cache.iterator().hasNext()) {
+            dstVertex = cache.iterator().next();
+          } else {
+            dstVertex = orientGraph.addVertex(null, "nodeId", key2);
+            vertices.put(key2, dstVertex);
+            orientGraph.commit();
+            nodesCounter++;
+          }
+
+          orientGraph.addEdge(null, srcVertex, dstVertex, "similar");
+          orientGraph.commit();
+
+          if (nodesCounter == 1000) {
+            duration = System.currentTimeMillis() - start;
+            insertionTimes.add((double) duration);
+            nodesCounter = 0;
+            start = System.currentTimeMillis();
+          }
+        }
+        lineCounter++;
+      }
+
+      duration = System.currentTimeMillis() - start;
+      insertionTimes.add((double) duration);
+      reader.close();
+    } catch (IOException ioe) {
+      System.out.println(ioe);
+      ioe.printStackTrace();
+    } catch (Exception e) {
+      System.out.println(e);
+      orientGraph.rollback();
+    }
+    Utils utils = new Utils();
+    utils.writeTimes(insertionTimes, OrientSingleInsertion.INSERTION_TIMES_OUTPUT_PATH + "." + count);
+  }
 }
--- src/eu/socialsensor/query/OrientQuery.java
+++ src/eu/socialsensor/query/OrientQuery.java
@@ -1,17 +1,13 @@
 package eu.socialsensor.query;
 
-import java.util.Iterator;
-import java.util.List;
-
+import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.tinkerpop.blueprints.Direction;
-import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.orient.OrientGraph;
-import com.tinkerpop.gremlin.java.GremlinPipeline;
-import com.tinkerpop.pipes.PipeFunction;
-import com.tinkerpop.pipes.branch.LoopPipe.LoopBundle;
-
+import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientVertex;
 import eu.socialsensor.benchmarks.FindShortestPathBenchmark;
+
+import java.util.List;
 
 /**
  * Query implementation for OrientDB graph database
@@ -19,59 +15,65 @@ import eu.socialsensor.benchmarks.FindShortestPathBenchmark;
  * @author sotbeis
  * @email sotbeis@iti.gr
  */
-public class OrientQuery  implements Query {
-	
-	private OrientGraph orientGraph = null;
-	
-	public static void main(String args[]) {
-	}
-	
-	
-	public OrientQuery(OrientGraph orientGraph) {
-		this.orientGraph = orientGraph;
-	}
-	
-	@Override
-	public void findNeighborsOfAllNodes() {
-		for(Vertex v : orientGraph.getVertices()) {
-			@SuppressWarnings("unused")
-			GremlinPipeline<String, Vertex> getNeighboursPipe = new GremlinPipeline<String, Vertex>(v).both("similar");
-		}
-	}
-	
-	@Override
-	public void findNodesOfAllEdges() {
-		for(Vertex v : orientGraph.getVertices()) {
-			for(Edge e : v.getEdges(Direction.BOTH)) {
-				GremlinPipeline<String, Vertex> getNodesPipe = new GremlinPipeline<String, Vertex>(e).bothV();
-				Iterator<Vertex> vertexIter = getNodesPipe.iterator();
-				@SuppressWarnings("unused")
-				Vertex startNode = vertexIter.next();
-				@SuppressWarnings("unused")
-				Vertex endNode = vertexIter.next();
-			}
-		}
-	}
-	
-	@Override
-	public void findShortestPaths() {
-		Vertex v1 = orientGraph.getVertices("nodeId", "1").iterator().next();
-		for(int i : FindShortestPathBenchmark.generatedNodes) {
-			final Vertex v2 = orientGraph.getVertices("nodeId", String.valueOf(i)).iterator().next();
-			@SuppressWarnings("rawtypes")
-			final GremlinPipeline<String, List> pathPipe = new GremlinPipeline<String, List>(v1)
-					.as("similar")
-					.out("similar")
-					.loop("similar", new PipeFunction<LoopBundle<Vertex>, Boolean>() {
-						//@Override
-						public Boolean compute(LoopBundle<Vertex> bundle) {
-							return bundle.getLoops() < 5 && !bundle.getObject().equals(v2);
-						}
-					})
-					.path();
-			@SuppressWarnings("unused")
-			int length = pathPipe.iterator().next().size();
-		}
-		
-	}
+public class OrientQuery implements Query {
+
+  private OrientBaseGraph orientGraph = null;
+
+  public OrientQuery(OrientBaseGraph orientGraph) {
+    this.orientGraph = orientGraph;
+  }
+
+  public static void main(String args[]) {
+  }
+
+  @Override
+  public void findNeighborsOfAllNodes() {
+    for (Vertex v : orientGraph.getVertices()) {
+      for (Vertex vv : v.getVertices(Direction.BOTH, "similar")) {
+
+      }
+      // GremlinPipeline<String, Vertex> getNeighboursPipe = new GremlinPipeline<String, Vertex>(v).both("similar");
+    }
+  }
+
+  @Override
+  public void findNodesOfAllEdges() {
+    for (Vertex v : orientGraph.getVertices()) {
+      for (Vertex vv : v.getVertices(Direction.BOTH)) {
+        // GremlinPipeline<String, Vertex> getNodesPipe = new GremlinPipeline<String, Vertex>(e).bothV();
+        // Iterator<Vertex> vertexIter = getNodesPipe.iterator();
+        // @SuppressWarnings("unused")
+        // Vertex startNode = vertexIter.next();
+        // @SuppressWarnings("unused")
+        // Vertex endNode = vertexIter.next();
+      }
+    }
+  }
+
+  @Override
+  public void findShortestPaths() {
+    Vertex v1 = orientGraph.getVertices("nodeId", "1").iterator().next();
+    for (int i : FindShortestPathBenchmark.generatedNodes) {
+      final Vertex v2 = orientGraph.getVertices("nodeId", String.valueOf(i)).iterator().next();
+
+      List<OrientVertex> result = orientGraph
+          .command(new OCommandSQL("SELECT shortestPath(" + v1.getId() + "," + v2.getId() + ")")).execute();
+      int length = result.size();
+
+      // @SuppressWarnings("rawtypes")
+      // final GremlinPipeline<String, List> pathPipe = new GremlinPipeline<String, List>(v1)
+      // .as("similar")
+      // .out("similar")
+      // .loop("similar", new PipeFunction<LoopBundle<Vertex>, Boolean>() {
+      // //@Override
+      // public Boolean compute(LoopBundle<Vertex> bundle) {
+      // return bundle.getLoops() < 5 && !bundle.getObject().equals(v2);
+      // }
+      // })
+      // .path();
+      // @SuppressWarnings("unused")
+      // int length = pathPipe.iterator().next().size();
+    }
+
+  }
 }
