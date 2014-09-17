3f32178959dbb7c2a9db352dea8f3a0e7db10ebc
sotbeis <sot.beis@gmail.com>
2014-09-17 15:35:49 +0300

    shortest path added

--- src/eu/socialsensor/query/OrientQuery.java
+++ src/eu/socialsensor/query/OrientQuery.java
@@ -1,16 +1,14 @@
 package eu.socialsensor.query;
 
-import com.orientechnologies.orient.core.command.OCommandRequest;
-import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.orient.OrientDynaElementIterable;
 import com.tinkerpop.blueprints.impls.orient.OrientExtendedGraph;
-import com.tinkerpop.blueprints.impls.orient.OrientVertex;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
+import com.tinkerpop.pipes.PipeFunction;
+import com.tinkerpop.pipes.branch.LoopPipe.LoopBundle;
 
 import eu.socialsensor.benchmarks.FindShortestPathBenchmark;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -20,48 +18,51 @@ import java.util.List;
  * @email sotbeis@iti.gr
  */
 public class OrientQuery implements Query {
+	private OrientExtendedGraph orientGraph = null;
 
-  private OrientExtendedGraph orientGraph = null;
+	public OrientQuery(OrientExtendedGraph orientGraph) {
+		this.orientGraph = orientGraph;
+	}
 
-  public OrientQuery(OrientExtendedGraph orientGraph) {
-    this.orientGraph = orientGraph;
-  }
+	public static void main(String args[]) {
+	}
 
-  public static void main(String args[]) {
-  }
+	@Override
+	@SuppressWarnings("unused")
+	public void findNeighborsOfAllNodes() {
+		for (Vertex v : orientGraph.getVertices()) {
+			for (Vertex vv : v.getVertices(Direction.BOTH, "similar")) {
+			}
+		}
+	}
 
-  @Override
-  public void findNeighborsOfAllNodes() {
-    for (Vertex v : orientGraph.getVertices()) {
-      for (Vertex vv : v.getVertices(Direction.BOTH, "similar")) {
-      }
-    }
-  }
+	@Override
+	@SuppressWarnings("unused")
+	public void findNodesOfAllEdges() {
+		for (Vertex v : orientGraph.getVertices()) {
+			for (Vertex vv : v.getVertices(Direction.BOTH)) {
+			}
+		}
+	}
 
-  @Override
-  public void findNodesOfAllEdges() {
-    for (Vertex v : orientGraph.getVertices()) {
-      for (Vertex vv : v.getVertices(Direction.BOTH)) {
-      }
-    }
-  }
-
-  @Override
-  public void findShortestPaths() {
-    Vertex v1 = orientGraph.getVertices("nodeId", "1").iterator().next();
-    for(int i = 0; i < 100; i++) {
-//    for (int i : FindShortestPathBenchmark.generatedNodes) {
-      final Vertex v2 = orientGraph.getVertices("nodeId", String.valueOf(i)).iterator().next();
-
-      OrientDynaElementIterable result = orientGraph
-          .command(new OCommandSQL("SELECT shortestPath(" + v1.getId() + "," + v2.getId() + ")")).execute();
-      
-      Iterator<Object> iter = result.iterator();
-      while(iter.hasNext()) {
-    	  Vertex v = (Vertex)iter.next();
-    	  System.out.println(v);
-      }
-    }
-
-  }
+	@Override
+	public void findShortestPaths() {
+		Vertex v1 = orientGraph.getVertices("nodeId", "1").iterator().next();
+		for(int i : FindShortestPathBenchmark.generatedNodes) {
+			final Vertex v2 = orientGraph.getVertices("nodeId", i).iterator().next();
+			@SuppressWarnings("rawtypes")
+			final GremlinPipeline<String, List> pathPipe = new GremlinPipeline<String, List>(v1)
+					.as("similar")
+					.out("similar")
+					.loop("similar", new PipeFunction<LoopBundle<Vertex>, Boolean>() {
+						//@Override
+						public Boolean compute(LoopBundle<Vertex> bundle) {
+							return bundle.getLoops() < 5 && !bundle.getObject().equals(v2);
+						}
+					})
+					.path();
+			@SuppressWarnings("unused")
+			int length = pathPipe.iterator().next().size() - 1;
+		}
+	}
 }
