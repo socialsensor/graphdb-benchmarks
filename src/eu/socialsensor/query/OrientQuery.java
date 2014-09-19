e210b3931e41c88a5042e9b69a81eaf970232986
sotbeis <sot.beis@gmail.com>
2014-09-19 15:37:27 +0300

    replace gremlin shortest path impl

--- src/eu/socialsensor/query/OrientQuery.java
+++ src/eu/socialsensor/query/OrientQuery.java
@@ -1,8 +1,12 @@
 package eu.socialsensor.query;
 
+import com.orientechnologies.orient.core.command.OBasicCommandContext;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.graph.sql.functions.OSQLFunctionShortestPath;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientExtendedGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientVertex;
 import com.tinkerpop.gremlin.java.GremlinPipeline;
 import com.tinkerpop.pipes.PipeFunction;
 import com.tinkerpop.pipes.branch.LoopPipe.LoopBundle;
@@ -47,22 +51,35 @@ public class OrientQuery implements Query {
 
 	@Override
 	public void findShortestPaths() {
-		Vertex v1 = orientGraph.getVertices("nodeId", "1").iterator().next();
-		for(int i : FindShortestPathBenchmark.generatedNodes) {
-			final Vertex v2 = orientGraph.getVertices("nodeId", i).iterator().next();
-			@SuppressWarnings("rawtypes")
-			final GremlinPipeline<String, List> pathPipe = new GremlinPipeline<String, List>(v1)
-					.as("similar")
-					.out("similar")
-					.loop("similar", new PipeFunction<LoopBundle<Vertex>, Boolean>() {
-						//@Override
-						public Boolean compute(LoopBundle<Vertex> bundle) {
-							return bundle.getLoops() < 5 && !bundle.getObject().equals(v2);
-						}
-					})
-					.path();
-			@SuppressWarnings("unused")
-			int length = pathPipe.iterator().next().size() - 1;
-		}
+		
+		 OrientVertex v1 = (OrientVertex) orientGraph.getVertices("nodeId", 1).iterator().next();
+	      for (int i : FindShortestPathBenchmark.generatedNodes) {
+	        final OrientVertex v2 = (OrientVertex) orientGraph.getVertices("nodeId", i).iterator().next();
+
+	        List<OIdentifiable> result = (List<OIdentifiable>) new OSQLFunctionShortestPath()
+	        .execute(orientGraph, null, null, new Object[] { 
+	        		v1.getRecord(), v2.getRecord(), Direction.OUT, 5 }, 
+	        		new OBasicCommandContext());
+
+	        int length = result.size();
+	      }
+		
+//		Vertex v1 = orientGraph.getVertices("nodeId", "1").iterator().next();
+//		for(int i : FindShortestPathBenchmark.generatedNodes) {
+//			final Vertex v2 = orientGraph.getVertices("nodeId", i).iterator().next();
+//			@SuppressWarnings("rawtypes")
+//			final GremlinPipeline<String, List> pathPipe = new GremlinPipeline<String, List>(v1)
+//					.as("similar")
+//					.out("similar")
+//					.loop("similar", new PipeFunction<LoopBundle<Vertex>, Boolean>() {
+//						//@Override
+//						public Boolean compute(LoopBundle<Vertex> bundle) {
+//							return bundle.getLoops() < 5 && !bundle.getObject().equals(v2);
+//						}
+//					})
+//					.path();
+//			@SuppressWarnings("unused")
+//			int length = pathPipe.iterator().next().size() - 1;
+//		}
 	}
 }
